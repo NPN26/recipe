654ee7749a5121e2617831dbc39fd810a1f595dd
NPN26 <133509488+NPN26@users.noreply.github.com>
2024-09-21 16:46:37 +0400

    implemented recipe-submission form and proccessment into db, testing required

--- recipe_submission_process.php
+++ recipe_submission_process.php
@@ -4,35 +4,43 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (!$connection) {
         die("Connection failed: " . mysqli_connect_error());
     }
-    $userid = $connection->real_escape_string($_POST[userid]);
-    $recipeName = $connection->real_escape_string($_POST[recipeName]);
-    $recipeDescription = $connection->real_escape_string($_POST[recipeDescription]);
-    $prepTime = $connection->real_escape_string($_POST[prepTime]);
-    $cookTime = $connection->real_escape_string($_POST[cookTime]);
-    $servings = $connection->real_escape_string($_POST[recipeServings]);
-    $recipeIngredients = $connection->real_escape_string($_POST[recipeIngredients]);
-    $ingredientQuantity = $connection->real_escape_string($_POST[ingredientsQuantity]);
-    $recipeInstructions = $connection->real_escape_string($_POST[recipeInstructions]);
+    $userid = $connection->real_escape_string($_POST["userId"]);
+    $recipeName = $connection->real_escape_string($_POST["recipeName"]);
+    $recipeDescription = $connection->real_escape_string($_POST["recipeDescription"]);
+    $prepTime = $connection->real_escape_string($_POST["prepTime"]);
+    $cookTime = $connection->real_escape_string($_POST["cookTime"]);
+    $servings = $connection->real_escape_string($_POST["recipeServings"]);
+    $recipeImage = $connection->real_escape_string($_POST["recipeImage"]);
+    $recipeIngredients = $_POST['recipeIngredients'];
+    $recipeInstructions = $_POST["recipeInstructions"];
     $created_at = date("Y-m-d H:i:s");
     
-    $sql = "INSERT INTO recipes (recipe_name, description, prep_time, cook_time, servings, created_at, user_id) VALUES ('$recipeName', '$recipeDescription', '$prepTime', '$cookTime', '$servings', '$created_at', '$userid')";
+    $prepTime = !empty($prepTime) ? $prepTime : 0;
+    $cookTime = !empty($cookTime) ? $cookTime : 0;
+    $servings = !empty($servings) ? $servings : 0;
+
+    $sql = "INSERT INTO recipe (recipe_name, description, prep_time, cook_time, servings, created_at, user_id) VALUES ('$recipeName', '$recipeDescription', '$prepTime', '$cookTime', '$servings', '$created_at', '$userid')";
     $result = $connection->query($sql);
 
     if ($result) {
         $recipe_id = $connection->insert_id;
-        $ingredients = explode("\n", $recipeIngredients);
-        $quantities = explode("\n", $ingredientQuantity);
-        $instructions = explode("\n", $recipeInstructions);
-        foreach ($ingredients as $key => $ingredient) {
+
+        foreach ($recipeIngredients as $ingredient) {
             $ingredient = $connection->real_escape_string($ingredient);
-            $quantity = $connection->real_escape_string($quantities[$key]);
-            $sql = "INSERT INTO ingredients (recipe_id, ingredient, quantity) VALUES ('$recipe_id', '$ingredient', '$quantity')";
+            $sql = "INSERT INTO recipe_ingredient (recipe_id, ingredient) VALUES ('$recipe_id', '$ingredient')";
             $result = $connection->query($sql);
+            if (!$result) {
+                echo "Error: " . $sql . "<br>" . $connection->error;
+            }
         }
-        foreach ($instructions as $instruction) {
+        $stepCount = 1;
+        foreach ($recipeInstructions as $instruction) {
             $instruction = $connection->real_escape_string($instruction);
-            $sql = "INSERT INTO instructions (recipe_id, instruction) VALUES ('$recipe_id', '$instruction')";
+            $sql = "INSERT INTO recipe_step (recipe_id, step_number, instruction) VALUES ('$recipe_id', '$stepCount', '$instruction')";
             $result = $connection->query($sql);
+            if (!$result) {
+                echo "Error: " . $sql . "<br>" . $connection->error;
+            }
         }
         echo "Recipe submitted successfully";
     } else {
